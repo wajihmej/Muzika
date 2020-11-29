b923b174b1a202df3ac1a7f3a2045186ceaab0f0
Mejri Wajih <wajih.mejri@esprit.tn>
2020-11-29 14:48:11 +0100

    Register

--- app/src/main/java/tn/example/muzika/Register.java
+++ app/src/main/java/tn/example/muzika/Register.java
@@ -1,14 +1,78 @@
 package tn.example.muzika;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.json.JSONObject;
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.google.android.material.textfield.TextInputEditText;
+import org.w3c.dom.Text;
+
+import okhttp3.Headers;
+
+import tn.example.muzika.models.user;
 
 public class Register extends AppCompatActivity {
 
+    Button Register;
+    EditText username,email,tel,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        username = (EditText)findViewById(R.id.usernamereg);
+        email = (EditText)findViewById(R.id.emailreg);
+        tel = (EditText)findViewById(R.id.telreg);
+        password = (EditText)findViewById(R.id.passwordreg);
+
+        Register = findViewById(R.id.registerb);
+
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Register.this, HomePage.class);
+                user loggedUser = register(intent);
+            }
+        });
+
     }
+
+    user register(Intent intent)
+    {
+        AsyncHttpClient client = new AsyncHttpClient();
+        final user[] loggedUser = new user[1];
+        client.post("https://nameless-cliffs-25074.herokuapp.com/api/auth/signup",
+                "{\"username\" : \""+username.getText().toString()+"\",\n" +
+                        "    \"email\" : \""+email.getText().toString()+"\",\n" +
+                        "    \"password\" : \""+password.getText().toString()+"\"}"
+                , new JsonHttpResponseHandler() {
+
+
+                    @Override
+                    public void onSuccess(int statusCode, Headers headers, JSON json) {
+                        JSONObject userJson = json.jsonObject;
+                        loggedUser[0] = user.fromJson(userJson);
+                        Log.d("Json" , loggedUser[0].toString());
+                        startActivity(intent);
+                    }
+
+
+
+                    @Override
+                    public void onFailure(int statusCode, @Nullable Headers headers, String errorResponse, @Nullable Throwable throwable) {
+                        Log.d("DEBUG", errorResponse);
+                    }
+                });
+        return loggedUser[0];
+    }
+
 }
